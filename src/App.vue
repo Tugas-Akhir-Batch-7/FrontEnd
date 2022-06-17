e3f7caa6658dc9f78374e6d849c4afee4671a93a
hamz72 <ilham72alkautsar@gmail.com>
2022-06-17 17:49:35 +0700

    dashboard done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <style>
 @import './assets/style.css';
-body{
-  justify-content: center;
-  align-items: center;
-}
 </style>
 
 <template>
@@ -12,13 +8,13 @@ body{
 
   <div class="navbar">
 
-      <RouterLink to="/">Home</RouterLink>
+      <!-- <RouterLink to="/">Home</RouterLink> -->
       <RouterLink to="/login">Sign In</RouterLink>
-      <RouterLink to="/signup">Sign Up _ Data</RouterLink>
-      <RouterLink to="/signup_2">Sign Up _ KTP</RouterLink>
+      <RouterLink to="/signup">Sign Up</RouterLink>
+      <!-- <RouterLink to="/signup_2">Sign Up _ KTP</RouterLink>
       <RouterLink to="/signup_3">Sign Up _ PFP</RouterLink>
       <RouterLink to="/signup_4">Sign Up _ Verify</RouterLink>
-      <RouterLink to="/forgot_password">Lupa Pass</RouterLink>
+      <RouterLink to="/forgot_password">Lupa Pass</RouterLink> -->
 
   </div>
 
--- src/components/Murid/Daftar.vue
+++ src/components/Murid/Daftar.vue
@@ -1,6 +1,16 @@
 <style>
 @import'..\assets\dashboard.css';
 
+#table_header{
+    display: inline-flex;
+    width:33.3%;
+    height: 10%;
+    background-color: rgba(0, 0, 0, 0.5); 
+
+    justify-content: center;
+    align-items: center;
+}
+
 </style>
 
 <template>
@@ -37,26 +47,40 @@
 </div>
 <!-- navbar -->
 <div class="navigation">
-    <div>
-        <RouterLink to="/login" class="nav_button"> Daftar</RouterLink>
-    </div>
-
-    <div>
+        <RouterLink to="/login" class="nav_button"> Tugas</RouterLink>
+        <RouterLink to="/login" class="nav_button"> Jadwal</RouterLink>
         <RouterLink to="/login" class="nav_button"> Profil</RouterLink>
-    </div>
-
-    <div>
+        <RouterLink to="/login" class="nav_button"> Administrasi</RouterLink>
         <RouterLink to="/login" class="nav_button"> Log Out</RouterLink>
-    </div>
+ 
 </div>
 
-
-<!-- sidebar -->
-<div class="sidebar">
-
+<!-- content -->
+<div style="display: inline-flex; justify-content: space-around;">
+    <!-- sidebar -->
+        <span class="sidebar">
+            <div id="sidebar_button">
+                Session
+            </div>
+            <div id="sidebar_button">
+                Absensi
+            </div>
+            <div id="sidebar_button">
+                Ujian
+            </div>
+        </span>
+        <span class="main_window">
+            <div id="table_header" style="border-top-left-radius: 25px;">
+                Hari, Tanggal
+            </div>
+            <div id="table_header">
+                Jam
+            </div>
+            <div id="table_header" style="border-top-right-radius: 25px;">
+                Materi
+            </div>
+        </span>
 </div>
-
-<!-- main window -->
 
 </template>
 
